6727251f341eec523c0f02bc7bce78637d356e01
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-04-06 16:24:12 +0400

    create new component folder & remove border from button

---
+++ src/components/text-content/TextContent.js
@@ -0,0 +1,13 @@
+import { faBox } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import "./TextContent.css";
+
+const TextContent = () => {
+    return(
+        <div className="textbox">
+        
+        </div>
+    )
+}
+
+export default TextContent;
