a23ab67fce164285d8720a41d16658e39fb6a49f
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-04-06 19:04:31 +0400

    add 2page

--- src/components/news/Newcontent.js
+++ src/components/news/Newcontent.js
@@ -6,11 +6,16 @@ const Newcontent = () => {
     return (
         <div className="newsInfo">
             <div className="newsContent">
-            <div className="circle"></div>
-            <h2>სიახლეები</h2>
+                <div className="circle"></div>
+                <h2>სიახლეები</h2>
             </div>
             <div className="content-Boxes">
-            {/* <TextContent /> */}
+                <TextContent />
+                <TextContent />
+                <TextContent />
+            </div>
+            <div className="moreRead">
+                <button className="btn3">მეტის ნახვა</button>
             </div>
         </div>
     )
