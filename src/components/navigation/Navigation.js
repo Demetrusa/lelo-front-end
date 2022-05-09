e9a9965aff55502b562b9ddf6ef47b1650a676d4
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-05-10 01:43:29 +0400

    fix: burgerbar

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,32 +1,82 @@
-import React from "react";
-import "./Navigation.css"
-import logo from "../../images/Layer_-1.svg"
-import globe from "../../images/globe.svg"
+import React, { useState } from "react";
+import "./Navigation.css";
+import logo from "../../images/Layer_-1.svg";
+import globe from "../../images/globe.svg";
 import Button from "../button/Button";
 
-const Navigation = ({flex}) => {
-    return (
-        <div className={flex ? "flex" : "nav"}>
-            <div className="nav-image">
-                <img src={logo} alt="showLogo" /> 
-            </div>
-                <div className="navigation">
-                    <div className="globe-logo">
-                        <img src={globe} alt="show globe"  id="nav-logo" />
-                    </div>
-                    <nav>
-                        <ul>
-                            <li><a href="/">მთავარი</a></li>
-                            <li><a href="/news">სიახლეები</a> </li>
-                            <li><a href="/more-teams">გუნდი</a> </li>
-                            <li><a href="/aboutUS">ჩვენს შესახებ</a></li>
-                            <li><a href="/donation">დონაცია</a> </li>
-                            <li> <a href="/register">შემოგვიერთდით</a></li>
-                        </ul>
-                    </nav>
-                </div>
+const Navigation = ({ flex }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={flex ? "flex" : "nav"}>
+      <div className="nav-image">
+        <img src={logo} alt="showLogo" />
+      </div>
+      <div className="navigation">
+        <div className="globe-logo">
+          <img src={globe} alt="show globe" id="nav-logo" />
         </div>
-    )
-}
+        <nav>
+          <ul>
+            <li>
+              <a href="/">მთავარი</a>
+            </li>
+            <li>
+              <a href="/news">სიახლეები</a>{" "}
+            </li>
+            <li>
+              <a href="/more-teams">გუნდი</a>{" "}
+            </li>
+            <li>
+              <a href="/aboutUS">ჩვენს შესახებ</a>
+            </li>
+            <li>
+              <a href="/donation">დონაცია</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="/register">შემოგვიერთდით</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+   
+          <div>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+      </div>
+      {open && (
+        <div className="hidden-menu">
+          <nav>
+            <ul>
+              <li>
+                <a href="/">მთავარი</a>
+              </li>
+              <li>
+                <a href="/news">სიახლეები</a>{" "}
+              </li>
+              <li>
+                <a href="/more-teams">გუნდი</a>{" "}
+              </li>
+              <li>
+                <a href="/aboutUS">ჩვენს შესახებ</a>
+              </li>
+              <li>
+                <a href="/donation">დონაცია</a>{" "}
+              </li>
+              <li>
+                {" "}
+                <a href="/register">შემოგვიერთდით</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Navigation;
